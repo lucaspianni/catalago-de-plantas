eb780780f87b172b0b517fe433a3a5c59b92cf98
Lucas <lucassantos2015lcs@gmail.com>
2026-08-13 16:57:02 -0300

    Update Next.js configuration for export and basePath

--- project/next.config.js
+++ project/next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/catalago-de-plantas',
+  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
